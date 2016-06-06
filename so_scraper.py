09b8bc51e6603ac2150ae272458e5905281cff07
Saad Qureshi <msaadq94@gmail.com>
2016-06-06 17:56:42 +0500

    Adds comments

--- so_scraper.py
+++ so_scraper.py
@@ -3,14 +3,24 @@ from lxml import html
 
 
 class so_scraper:
-    base_url = "https://stackoverflow.com"
-    login_url = base_url + "/users/login"
-    session_request = []
-    credentials = []
-    main_page = []
-    questions = []
+    base_url = "https://stackoverflow.com"  # Base URL for the SO website
+    login_url = base_url + "/users/login"  # Login page URL
+    session_request = []  # Independent instance of a web session request
+    credentials = []  # Dictionary of login credentials with auth token
+    main_page = []  # Main HTML page saved after successful login
+    questions = []  # HTML element containing the sub elements of all the top questions belonging to class "question-mini-list"
+    page_title = ""  # Title of the current page
 
     def __init__(self, email, password):
+
+        """
+        Initiates the session and Logs in with the provided credentials
+
+        :param email:
+        :param password:
+        :return:
+        """
+
         self.session_request = requests.session()
         result = self.session_request.get(self.login_url)
         login_page = html.fromstring(result.text)
@@ -29,24 +39,62 @@ class so_scraper:
         )
 
         self.main_page = html.fromstring(result.content)
-        self.questions = self.main_page.xpath("//div[@id='question-mini-list']")[0]
+        self.page_title = str(self.main_page.xpath("//title/text()")[0])
 
-    def get_reputation(self):
+        if self.page_title == "Stack Overflow":
+            print "Login Successful" + "\n"
+            self.questions = self.main_page.xpath("//div[@id='question-mini-list']")[0]
+        else:
+            print "Invalid Credentials" + "\n"
+
+    def _get_reputation(self):
+
+        """
+        Parses the HTML page for reputation score
+
+        :return: reputation as int
+        """
+
         return int(str(self.main_page.xpath('//span[@class="reputation"]/text()')[0]).strip())
 
-    def get_ques_titles(self):
+    def _get_ques_titles(self):
+
+        """
+        Parses the HTML page for all "Top Questions" titles
+
+        :return: list of titles
+        """
+
         return self.questions.xpath("//a[@class='question-hyperlink']/text()")
 
-    def get_ques_links(self):
+    def _get_ques_links(self):
+
+        """
+        Parses the HTML page for all "Top Questions" titles
+
+        :return: list of questions links
+        """
+
         return [self.base_url + link for link in self.questions.xpath("//a[@class='question-hyperlink']/@href")]
 
     def display_data(self):
-        print "Reputation: " + str(self.get_reputation()) + "\n"
 
-        ques_titles = self.get_ques_titles()
-        ques_links = self.get_ques_links()
+        """
+        Displays the reputation score along with all top questions and links in a neat format
 
-        for i in xrange(len(ques_titles)):
-            print "Question: " + str(i + 1)
-            print ques_titles[i]
-            print ques_links[i] + "\n"
+        :return: none
+        """
+
+        try:
+            print "Reputation: " + str(self._get_reputation()) + "\n"
+
+            ques_titles = self._get_ques_titles()
+            ques_links = self._get_ques_links()
+
+            for i in xrange(len(ques_titles)):
+                print "Question: " + str(i + 1)
+                print ques_titles[i]
+                print ques_links[i] + "\n"
+
+        except:
+            print "You are not logged in!"
