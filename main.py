edee4d3ddb6734838ccaebeba80b8467136fcee0
Saad Qureshi <msaadq94@gmail.com>
2016-06-06 18:08:07 +0500

    Moves the relevant files inside the src folder

--- main.py
+++
@@ -1,5 +0,0 @@
-import so_scraper as scrap
-
-scrapper = scrap.so_scraper("msaadq94@gmail.com", "samplepasswordforCarbyne")
-scrapper.display_data()
-
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import so_scraper as scrap
+
+# Enter Email and Password here
+
+email = ""
+password = ""
+
+scrapper = scrap.so_scraper(email, password)
+scrapper.display_data()
+
